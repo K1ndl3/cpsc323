6224120afc5434d5589a0692cba3f8e95e20197c
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-09-19 19:44:46 -0700

    added a class and some todos

---
+++ Lexer.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class Lexer {
+public:
+    // TODO: implement the rule of 5 for the lexer class
+private:
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
+    // TODO: implement file I/O for the rat25F text file
     std::cout << "Hello world";
     return 0;
 }
