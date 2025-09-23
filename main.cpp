40024df8253944c8a89143386793cab5b06385f5
JasonDaRuler <khaujason@hotmail.com>
2025-09-23 15:37:36 -0700

    Added operator set

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <unordered_set>
 #include <string>
 int main() {
-    std::unordered_set<std::string> keywordSet{"true", "false", "integer", "if", "else", "fi", "wihle", "return", "get", "put"};
+    std::unordered_set<std::string> keywordSet = {"true", "false", "integer", "if", "else", "fi", "wihle", "return", "get", "put"};
+    std::unordered_set<std::string> operatorSet = {"==", "!=", ">", "<", ">=", "<=", "+", "-", "*", "/"};
     // TODO: implement file I/O for the rat25F text file
-    std::cout << "Hello world";
+    std::string sample = "true";
+    std::cout << keywordSet.count(sample);
     return 0;
 }
