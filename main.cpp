8425f3b37dbb2da8a8978882824b61a831a220b4
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-09-28 22:59:10 -0700

    committed by: -Huy Bui what was achieved: - Added File IO comments/concerns: - Next todo should be on building the lexer   function with helper functions like isDigit()

--- main.cpp
+++ main.cpp
@@ -2,11 +2,33 @@
 #include "./Lexer-folder/Lexer.hpp"
 #include <unordered_set>
 #include <string>
+#include <fstream>
+
+//////////////////////////////////////////// Look Up Tables ////////////////////////////////////////////////
+const std::unordered_set<std::string> keywordSet = {"true", "false", "integer", "if", "else", "fi", "while", "return", "get", "put"};
+const std::unordered_set<std::string> operatorSet = {"==", "!=", ">", "<", ">=", "<=", "+", "-", "*", "/"};
+
+
 int main() {
-    std::unordered_set<std::string> keywordSet = {"true", "false", "integer", "if", "else", "fi", "wihle", "return", "get", "put"};
-    std::unordered_set<std::string> operatorSet = {"==", "!=", ">", "<", ">=", "<=", "+", "-", "*", "/"};
-    // TODO: implement file I/O for the rat25F text file
-    std::string sample = "true";
-    std::cout << keywordSet.count(sample);
+    std::cout << "Lexer for rat24F starting\n";
+    
+    // Taking input from input.txt and storing into inputFile
+    std::ifstream inputFile("input.txt");
+    if (!inputFile) {
+        std::cout << "[ERROR] -> cannot open file " << "'input.txt'\n";
+        return 1;
+    }
+    
+    // Input file sucessfully loaded
+    std::cout << "[SUCCESS] -> file opened";
+    std::cout << "Running lexer...\n";
+    
+
+    // iterate through the inputFile using .get() and run lexer()
+    char ch;
+    while (inputFile.get(ch)) {
+        std::cout << ch << '\n';
+    }
+
     return 0;
 }
